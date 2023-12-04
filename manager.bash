d5c9eda8de2b333ecdf7d2f80f901832e1ccba81
Jack Leys <jackal92591@gmail.com>
2023-12-04 14:00:10 -0500

    gpio toggle

--- manager.bash
+++ manager.bash
@@ -3,6 +3,8 @@
 # Navigate to the directory where scripts are
 cd /home/capstone # I think this is right? dont have pi rn
 
+raspi-gpio set 5 op dh
+
 # Run grabber until it succeeds
 until python Grabber_Scrape.py; do
     echo "Grabber failed, retrying..."
@@ -11,5 +13,8 @@ done
 
 # Run the rest of the scripts in order
 python Combine_Binary2.py
+
+export BOKEH_BROWSER=/usr/bin/chromium-browser
+
 python Server.py
 python FlaskRun.py
