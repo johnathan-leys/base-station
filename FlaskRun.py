74e0a2c3429b4ffbc9cc0eb53777a47c3a872e2b
Jack Leys <118081373+johnathan-leys@users.noreply.github.com>
2024-02-22 15:27:22 -0500

    Update FlaskRun.py comments

--- FlaskRun.py
+++ FlaskRun.py
@@ -1,15 +1,17 @@
 # testing with flask
 from flask import Flask, render_template_string
 
-app = Flask(__name__)
+# Create instance "app" of the Flask class
+app = Flask(__name__)    # __name__ = name of current module
 
-@app.route('/')
-def index():
+# Decorator, tells Flask to associate "index" function with url '/'. When request is made to '/', function is invoked
+@app.route('/')    # '/' is usually default from browser
+def index():        # Funtion reads,renders spectrogam file
     with open("interactive_spectrogram.html", 'r') as f:
         content = f.read()
     return render_template_string(content)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)    # Accesible from any net interface, port 5k
 
 # Seems to work. Def need to optimize/learn more about web stuff to load faster.
